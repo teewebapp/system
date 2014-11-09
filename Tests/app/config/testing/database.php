3c8c51fa16da648856acfc640184700b91e4f5ed
Anderson Danilo <contato@andersondanilo.com>
2014-11-09 16:56:10 -0200

    correção teste unitário

--- Tests/app/config/testing/database.php
+++ Tests/app/config/testing/database.php
@@ -48,7 +48,7 @@ return array(
 
 		'sqlite' => array(
 			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/database.sqlite',
+			'database' => __DIR__.'/../../database/database.sqlite',
 			'prefix'   => '',
 		),
 		/*
